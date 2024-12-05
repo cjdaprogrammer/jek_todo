6ff16fb1a137d63e7219b2661d5d69b816e4cc85
Clark James De Luna <163956768+cjdaprogrammer@users.noreply.github.com>
2024-12-05 12:07:40 +0800

    Add files via upload

---
+++ personal_todo/todo/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
+
+def task_list(request):
+    tasks = Task.objects.filter(completed=False)  # Exclude completed tasks
+    return render(request, 'todo/task_list.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            Task.objects.create(title=title)
+    return redirect('task_list')
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('task_list')
+
+def toggle_completed(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.completed = not task.completed
+    task.save()
+    return redirect('task_list')
+
+def edit_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            task.title = title
+            task.save()
+    return redirect('task_list')
